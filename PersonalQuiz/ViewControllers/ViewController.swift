492d956e6b580b085b08be41e483cd6f006e3c34
Shamkhan Mutuskhanov <inkomman@gmail.com>
2023-05-30 14:29:52 +0300

    Initial Commit

---
+++ PersonalQuiz/ViewControllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalQuiz
+//
+//  Created by Shamkhan Mutuskhanov on 26.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    private let questions = Question.getQuestions()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
